cdfbd38759cbd1a2c383d49684faba4602a16102
Tonniekangethe <tonniekamau64@gmail.com>
2023-03-31 15:58:18 -0700

    leetcode

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * cap_string - write characters in upper case
+ * @str: string
+ *
+ * Return: value of string
+ */
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
+			str[i]++;
+
+		if (str[i - 1] == ' ' ||
+				str[i - 1] == '\t' ||
+				str[i - 1] == '\n' ||
+				str[i - 1] == ',' ||
+				str[i - 1] == ';' ||
+				str[i - 1] == '.' ||
+				str[i - 1] == '!' ||
+				str[i - 1] == '?' ||
+				str[i - 1] == '"' ||
+				str[i - 1] == '(' ||
+				str[i - 1] == ')' ||
+				str[i - 1] == '{' ||
+				str[i - 1] == '}' ||
+				i == 0)
+			str[i] = str[i] - 32;
+		i++;
+
+	}
+	return (str);
+}
