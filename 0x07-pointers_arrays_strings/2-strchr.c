98d7c946abb78f6b722c0cbb3e293eafc7946d9c
Tonniekangethe <tonniekamau64@gmail.com>
2023-04-04 05:53:00 -0700

    _strchr function

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,12 +9,14 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i;
+	unsigned int i;
 
 	for (i = 0 ; s[i] != '\0' ; i++)
 	{
 		if (s[i] == c)
 			return (s + i);
+		if (c == '\0')
+			return (s);
 	}
 		return ('\0');
 }
