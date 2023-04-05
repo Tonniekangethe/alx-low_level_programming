ae460d53c917382e83ae8548fc0de8c394a653ea
Tonniekangethe <tonniekamau64@gmail.com>
2023-04-05 12:49:01 -0700

    factorial recursion

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * factorial - the sum of multiplication of all the positive integers smaller than n
+ * @n: the input value
+ *
+ * Return: factorial of n
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n <= 1)
+		return (1);
+	return (n * factorial(n - 1));
+}
