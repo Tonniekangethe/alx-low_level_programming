c01fb6cb4d293e082d5ce0a5c82eefb1c4fd23e7
Tonniekangethe <tonniekamau64@gmail.com>
2023-04-06 13:46:30 -0700

    squareroot recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-int natural_sqrt_recursion(int r, int n);
+int natural_sqrt_recursion(int n, int r);
 /**
  * _sqrt_recursion - square root of the recursive function
  * @n: integer value
@@ -11,7 +11,7 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	return (natural_sqrt_recursion(n, 0));
+	return (natural_sqrt_recursion(n, 1));
 }
 /**
  * natural_sqrt_recursion - recursion function to find squareroot of r
@@ -20,11 +20,11 @@ int _sqrt_recursion(int n)
  *
  * Return: integer value
  */
-int natural_sqrt_recursion(int r, int n)
+int natural_sqrt_recursion(int n, int r)
 {
+	if (r * r > n)
+		return (-1);
 	if (r * r == n)
 		return (r);
-	if (r * r > n)
-		return (natural_sqrt_recursion(n, r - 1));
-	return (natural_sqrt_recursion(n, r + 1));
+	return ( natural_sqrt_recursion(n, r + 1));
 }
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+int is_prime_number(int n)
+{
+	if (n < 1)
+		return (0);
+	return (actual is_prime_number(n n - 1));
+}
+
