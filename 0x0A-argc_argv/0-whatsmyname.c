1277313f3368bc51f61e15e3dea91cc850e6a83e
Tonniekangethe <tonniekamau64@gmail.com>
2023-04-10 13:18:36 -0700

    my name is

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,11 +3,11 @@
 /**
  * main - the main function
  * @argc: number of command line arguments passed by the user
- * @argv[]: an array of characters pointers listing all arguments
+ * @argv: an array of characters pointers listing all arguments
  *
  * Return: 0 success
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
