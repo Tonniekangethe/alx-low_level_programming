3e952b87e92030427d2f47919d3a88e2efa5fec0
Tonniekangethe <tonniekamau64@gmail.com>
2023-04-10 14:05:44 -0700

    args strings

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ * @argc: number of command line arguments passed by the user
+ * @argv: an array of characters pointers listing all arguments
+ *
+ * Return: 0 success
+ */
+int main(int argc, char *argv[])
+{
+	int x;
+
+	for (x = 1 ; x < argc ; x++)
+	{
+		printf("%s\n", argv[x]);
+	}
+	return (0);
+}
