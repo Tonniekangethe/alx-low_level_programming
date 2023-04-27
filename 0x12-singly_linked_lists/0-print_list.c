a96c2951793502b6e91bdc22f75e4d109997d3af
Tonniekangethe <tonniekamau64@gmail.com>
2023-04-27 14:32:12 -0700

    print lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * print_list - a function that prints all elements in a list
+ * @h: pointer to a string
+ *
+ * Return: number of nodes printed
+ */
+size_t print_list(const list_t *h)
+{
+	size_t x = 0;
+
+	while (h)
+	{
+		if (!h -> str)
+			printf("[0](nil)\n");
+		else
+			printf("[%u] %s\n", h -> len, h -> str);
+		h = h -> next;
+		x++;
+	}
+
+       return (x);
+}
