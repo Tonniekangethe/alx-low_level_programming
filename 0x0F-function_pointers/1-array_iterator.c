96ca12cd811a864d804e0dcac217ec7fd62d8db3
Tonniekangethe <tonniekamau64@gmail.com>
2023-06-21 14:03:55 -0700

    changes

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,11 +10,11 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int i;
+	unsigned int i;
 
-	if (araay == NULL  || action == NULL)
+	if (array == NULL  || action == NULL)
 	{
-		return (NULL);
+		return;
 	}
 
 	for (i = 0; i < size; i++)
