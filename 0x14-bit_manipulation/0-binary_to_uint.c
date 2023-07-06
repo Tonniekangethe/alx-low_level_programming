15bc9d6fb7b9d97888821339755c5f17032ddbf7
Tonniekangethe <tonniekamau64@gmail.com>
2023-07-06 15:56:34 -0700

    convertion

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,14 +10,14 @@
 unsigned int binary_to_uint(const char *b)
 {
 	int i;
-	unsigned int val;
+	unsigned int val = 0;
 
 	if (!b)
 		return (0);
 	for (i = 0; b[i]; i++)
 	{
-		if (b[i] != '0' || b[1] != '1')
-			return (NULL);
+		if (b[i] < '0' || b[i] > '1')
+			return ('\0');
 		val = 2 * val + (b[i] - '0');
 	}
 	return (val);
