25f76b8a42ae193d7dc0249278447d1e61f97523
Tonniekangethe <tonniekamau64@gmail.com>
2023-08-10 11:31:30 -0700

    append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,6 +21,8 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	o = open(filename, O_WRONLY | O_APPEND);
 	w = write(o, text_content, l);
+	if (o == -1 || w == -1)
+		return (-1);
 
 	close(o);
 	return (1);
