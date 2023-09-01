0bd04388394db1c4410d5a6257f025e10cf7d2bd
Tonniekangethe <tonniekamau64@gmail.com>
2023-09-01 11:12:37 -0700

    correction

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,11 +13,13 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int num = 0;
 
 	if (b == NULL)
-		return ('0');
+		return (0);
+	if (b[0] == '\0')
+		return (0);
 	for ( ; b[i]; i++)
 	{
 		if (b[i] < '0' || b[i] > '1')
-			return ('\0');
+			return (0);
 		num = 2 * num + (b[i] - '0');
 	}
 	if (i == 0)
